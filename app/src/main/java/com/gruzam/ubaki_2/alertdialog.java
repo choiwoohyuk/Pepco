bb04b348420a4baa32ef7f14b749287eec1814e3
Hwang int <hwang9029@naver.com>
2017-09-27 21:08:54 +0900

    alertdialog fin

--- app/src/main/java/com/gruzam/ubaki_2/alertdialog.java
+++ app/src/main/java/com/gruzam/ubaki_2/alertdialog.java
@@ -21,9 +21,10 @@ public class alertdialog extends AppCompatActivity {
 
     int show_quiz;
 
-    String[] Question;
-    String[][] Quiz;
-    String[] Correct;
+    String[] Question = null;
+    String[][] Quiz = null;
+    String[] Correct = null;
+    String[] Explanation_arr = null;
 
     String[] parant_teaching = {
             "문제 1) 다음 중 알맞지 않는 것을 고르시오.",
@@ -103,7 +104,26 @@ public class alertdialog extends AppCompatActivity {
             "X"
     };
 
-    String[] Explanation_array = {"이부분에 해설"};
+    String[] Explanation_arr_parant_teaching = {
+            "묽은 변이 아닌 좀 더 단단한 변을 배설한다.",
+            "3 ~ 4세 정도가 되면 서너 개의 단어를 사용해서 문장을 형성하고, 단어도 1000개 정도에 이른다.",
+            "유아기 같은 급격한 신체발달이 일어나지 않는다."
+    };
+
+    String[] Explanation_arr_disease = {
+            "목욕은 너무 장시간 뜨거운 물에 노출시키지 말고 10~15분 내외로 짧게 마치는 것이 좋다. 또한 목욕을 자주 시켜주면 피부를 악화 시킬 수 있으니 특별한 이 없으면 일주일에 3,4회 정도 시켜주는 것이 좋다.",
+            "젖은 기저귀에 직접 닿는 하복부, 생식기 주변과 허벅지 윗부분이 빨갛게 변하거나 작은 반점이 생기는 것도 기저귀 발진을 의심해 볼 수 있다.",
+            "민간요법으로 소금물을 사용 시 가려움을 더 유발할 수 있다."
+    };
+
+    String[] Explanation_arr_child_safety = {
+            "두 젖꼭지 사이의 가상선 아래 흉골에 세 손가락을 두고 검지는 들어 올리고 두 손가락(중지와 약지)으로 흉골의 하부 1/3 부위를 압박한다.",
+            "청색증은 피부와 점막이 푸른색을 나타내는 것이므로 주로 분노 발작의 요소로서 또는 야단치거나 다른 속상한 경우에 대응하여 유아에서 잠재의식적으로 시작된다.",
+            "일반적으로 떨어져 머리를 부딪히거나 갑자기 깜짝 놀라는 등의 통증을 경험한 후 나타난다.",
+            "영유아에서 열성감염증이 있을 때 최고의 열 상승기에 전신 경련을 일으키는 경우이며, 모든 소아의 3 ~ 4%에서 경험한다고 한다. 하지만 간질이 될 가능성은 낮다.",
+            "의복을 벗겨 가슴을 열어 압박하지 않도록 한다.\n" + "머리는 차게 한다.\n" + "머리를 좀 높게한다.",
+            "말도안되는 방법이다 절대해선 안된다."
+    };
 
     public AlertDialog.Builder QuestionManual;
     public AlertDialog.Builder Explanation;
@@ -131,14 +151,20 @@ public class alertdialog extends AppCompatActivity {
                 Question = parant_teaching;
                 Quiz = parant_teaching_quiz;
                 Correct = parant_teaching_correct;
+                Explanation_arr = Explanation_arr_parant_teaching;
+                break;
             case 2:
                 Question = disease;
                 Quiz = disease_quiz;
                 Correct = disease_correct;
+                Explanation_arr = Explanation_arr_disease;
+                break;
             case 3:
                 Question = child_safety;
                 Quiz = child_safety_quiz;
                 Correct = child_safety_correct;
+                Explanation_arr = Explanation_arr_child_safety;
+                break;
         }
 
     }
@@ -168,16 +194,16 @@ public class alertdialog extends AppCompatActivity {
         if (Quiz[x][answer].equals(Correct[x])) {
             retValue = "정답입니다!";
             x++; // 다음 문제로 넘어감
-            if (x == Quiz.length) { // 다음 동영상 재생시 이 부분을 바꾸면 된다.
-                Button chang_button = (Button) findViewById(R.id.set_button);
+            if (x == Quiz.length) {                                                                     // 모든 문제를 풀면 문제 button이 'NEXT'로 txt가 변경되게 하였다.
+                Button chang_button = (Button) findViewById(R.id.set_button);                           // 따라서 if 안에 다음 영상을 call할 수 있게 추가하면 된다.
                 chang_button.setText("NEXT");
-                x = 0;
             }else {
                 Toast.makeText(getApplicationContext(), retValue, Toast.LENGTH_LONG).show();
                 makeQuiz();
             }
 
-            Explanation.setMessage(Explanation_array[0]);
+            Explanation.setMessage(Explanation_arr[x - 1]);
+            Explanation.setPositiveButton("확인", null);
             Explanation.create().show();
 
         } else {
