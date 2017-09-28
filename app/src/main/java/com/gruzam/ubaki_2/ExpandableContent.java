d10e123a6b754217c4e756d0113efc36adff3b91
ok <rmsdnsms@naver.com>
2017-09-28 15:27:53 +0900

    ok_geun_woo_expandableList

---
+++ app/src/main/java/com/gruzam/ubaki_2/ExpandableContent.java
@@ -0,0 +1,72 @@
+package com.gruzam.ubaki_2;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.ExpandableListView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+
+public class ExpandableContent extends Activity {
+
+    ExpandableListAdapter listAdapter;
+    ExpandableListView expListView;
+    List<String> listDataHeader;
+    HashMap<String, List<String>> listDataChild;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.expanable_layout);
+
+        // get the listview
+        expListView = (ExpandableListView) findViewById(R.id.lvExp);
+
+        // preparing list data
+        prepareListData();
+
+        listAdapter = new ExpandableListAdapter(this, listDataHeader, listDataChild);
+
+        // setting list adapter
+        expListView.setAdapter(listAdapter);
+
+        // Listview Group click listener
+
+        };
+    /*
+     * Preparing the list data
+     */
+    private void prepareListData() {
+        listDataHeader = new ArrayList<String>();
+        listDataChild = new HashMap<String, List<String>>();
+
+        // Adding child data
+        listDataHeader.add("부모교육");
+        listDataHeader.add("질병예방");
+        listDataHeader.add("안전사고");
+
+        // Adding child data
+        List<String> parentes = new ArrayList<String>();
+        parentes.add("영아기");
+        parentes.add("유아기");
+
+
+        List<String> prevention = new ArrayList<String>();
+        prevention.add("신생아 태열");
+        prevention.add("아토피 피부염");
+        prevention.add("기저귀 발진");
+        prevention.add("땀띠");
+        prevention.add("유아 변비");
+
+        List<String> accident = new ArrayList<String>();
+        accident.add("응급처치 (심장 마사지)");
+        accident.add("호흡정지 발작");
+        accident.add("열성경련");
+
+        listDataChild.put(listDataHeader.get(0), parentes); // Header, Child data
+        listDataChild.put(listDataHeader.get(1), prevention);
+        listDataChild.put(listDataHeader.get(2), accident);
+    }
+}
